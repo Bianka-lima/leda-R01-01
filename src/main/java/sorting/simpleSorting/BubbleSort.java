56eecc925b78267b6cc2f914ffe6b541e24565ba
124110621 BIANKA VITORIA SILVA LIMA <bianka.vitoria.silva.lima@lcc2-53.lcc.ufcg.edu.br>
2025-06-26 07:48:31 -0300

    bubbleSort, início

--- src/main/java/sorting/simpleSorting/BubbleSort.java
+++ src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -11,7 +11,17 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		boolean trocaOcorreu = true;
+		while (trocaOcorreu) {
+			trocaOcorreu = false;
+			for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i] > array[i+1]) {
+
+				}
+			}
+		}
+	}
+	public void troque(T[] array, int indexI, int indexJ) {
+
 	}
 }
